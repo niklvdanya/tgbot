25238895d875cb78a6e422362ce0d23d86372800
Daniil Nikolaev <quanttlight@gmail.com>
2023-12-15 18:33:50 +0300

    no add

--- Bot.cpp
+++ Bot.cpp
@@ -1,20 +1,26 @@
 #include "Bot.h"
 #include <iostream>
 
-void Bot::run() {
-    try {
+void Bot::run()
+{
+    try 
+    {
         std::cout << "Bot username: " << bot.getApi().getMe()->username << std::endl;
         TgBot::TgLongPoll longPoll(bot);
-        while (true) {
+        while (true) 
+        {
             std::cout << "Long poll started" << std::endl;
             longPoll.start();
         }
-    } catch (TgBot::TgException& e) {
+    } 
+    catch (TgBot::TgException& e) 
+    {
         std::cout << "error: " << e.what() << std::endl;
     }
 }
 
 
-void Bot::setupHandlers() {
+void Bot::setupHandlers() 
+{
     messageHandlers_.setupHandlers();
 }
